1c8191c10e34c291ab5d366b578972a788d2f68c
Galina094 <102922761+Galina094@users.noreply.github.com>
2023-10-12 19:58:26 +0300

    HW3

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function userName() {
+    let uName = prompt("Давай знакомиться! Я - Клаус, а ты?", "Введите своё имя"), id = "demo";
+    alert (`Привет, ${uName}!`);     
+    return uName;    
+}
+
+
+
+
+
+
